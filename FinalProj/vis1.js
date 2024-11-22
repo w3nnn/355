9a07c1b8bd1f227e27c1cbd028659011abdc6b0e
w3nnn <95375266+w3nnn@users.noreply.github.com>
2024-11-21 20:41:59 -0800

    Mi final proj

---
+++ FinalProj/vis1.js
@@ -0,0 +1,25 @@
+
+// async function render() {
+
+// const data = await d3.csv("Drug use disorder death rate - Sheet3.csv");
+//   console.log(data);
+
+// const chart1Spec = {
+//     $schema: "https://vega.github.io/schema/vega-lite/v5.json",
+//     description: "A bar chart of all the countries",
+//     width: "container", 
+//     height: 300, 
+//     data: { url: "Drug use disorder death rate - Sheet3.csv" }, 
+//     mark: "bar",
+//     encoding: {
+//       y: { field: "location.Canada", type: "nominal" },
+//       x: { field: "val", type: "quantitative" }
+//     }
+//   };
+
+
+//   vegaEmbed("#vis1", chart1Spec);
+// }
+
+
+
